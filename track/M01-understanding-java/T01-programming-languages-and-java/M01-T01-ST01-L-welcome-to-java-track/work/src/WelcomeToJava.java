ef883116d51b537a5725069b88da7700c9283772
viswateja <viswateja2828@gmail.com>
2026-07-30 12:52:29 +0530

    Day1

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,5 +1,9 @@
+
 public class WelcomeToJava {
+
     public static void main(String[] args) {
-       //Write your code here
+        System.out.println("Welcome to the java track");
+        System.out.println("I already know how to build programming logic");
+        System.out.println("Now i am learning how to express that logic using java");
     }
 }
